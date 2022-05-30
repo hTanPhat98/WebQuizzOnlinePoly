3e00378dc571ff4984b78711db716b466a22f47f
hTanPhat98 <94031544+hTanPhat98@users.noreply.github.com>
2022-05-30 23:04:29 +0700

    Up Project

---
+++ WebQuizzOnline/db/HoiDap.js
@@ -0,0 +1,12 @@
+[
+    {   
+        "Id": "1",
+        "Ch": "Câu 1: Ngành Công nghệ thông tin là gì?",
+        "Tl": "- Công nghệ thông tin là một thuật ngữ bao gồm phần mềm, mạng lưới Internet, hệ thống máy tính sử dụng cho việc phân phối và xử lý dữ liệu, trao đổi, lưu trữ và sử dụng thông tin dưới hình thức khác nhau.Nói nôm na, đây là việc sử dụng công nghệ hiện đại vào việc tạo ra, xử lý, truyền dẫn thông tin, lưu trữ, khai thác thông tin.Hiện nay, ngành máy tính và CNTT hay được gọi chung là CNTT 1 thường phân chia thành các chuyên ngành phổ biến: Khoa học máy tính, kỹ thuật máy tính, hệ thống thông tin, mạng máy tính truyền thông, kỹ thuật phần mềm."
+    },
+    {   
+        "Id": "1",
+        "Ch": "Câu 2: Khi theo học ngành nàysinh viên sẽ được học những gì?",
+        "Tl": "- Học ngành Công nghệ thông tin, sinh viên được trang bị các kiến thức nền tảng về khoa học tự nhiên, kiến thức cơ bản như mạng máy tính, hệ thống thông tin, lập trình, phần mềm… Đồng thời tùy theo chương trình đào tạo của từng trường đại học, người học sẽ được chọn học các chuyên ngành phù hợp với năng lực, sở thích như Công nghệ phần mềm, Khoa học máy tính, Hệ thống thông tin, An toàn thông tin, Mạng máy tính và truyền thông...Ví dụ như đối với ngành Kỹ thuật phần mềm, sinh viên sẽ được học nhóm kiến thức như sau: - Kiến thức cơ bản về CNTT -Quy trình phát triển phần mềm, từ phương pháp, kỹ thuật, công nghệ trong phân tích, thiết kế, phát triển, kiểm thử, bảo trì phần mềm và quản lý dự án phần mềm… - Ngoài ra, sinh viên có tối thiểu một học kỳ được học tập và đào tạo thực tế tại các công ty phần mềm có tiếng trong nước, có cơ hội học hỏi từ các chuyên gia đầu ngành."
+    }
+]
